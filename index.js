85fa0d8de0703b5a02a5af6df05a4d1007c9f170
jamesdai05 <dai_guilong@hotmail.com>
2024-05-18 00:32:12 +0800

    add the logic

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
 // console.log("Hello");
 // alert("Hello, welcome to the page!");
+
+//retrieved the items from local-storage or initialize the todo;
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+const todoInput = document.querySelector(".input-field");
+console.log(todoInput);
+
+const button = document.querySelector(".btn");
+const todoList = document.querySelector("#todoList");
+const itemCount = document.querySelector(".todoCount");
+
+const deleteButton = document.querySelector("#deleteAll");
+
+// console.log(todoInput.value);
+// console.log(button);
+
+//Initialization
+
+document.addEventListener("DOMContentLoaded", () => {
+  // console.log('clicked');
+  button.addEventListener("click", addTodo);
+  todoInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      addTodo();
+    }
+  });
+  // deleteButton.addEventListener("click", deleteAllItems());
+  // displayItems();
+});
+
+function addTodo() {
+  // console.log("click");
+  console.log(todoInput.value);
+  const newItem = todoInput.value.trim();
+  if (newItem !== "") {
+    todo.push({ text: newItem, disabled: false });
+    saveToLocalStorage();
+    todoInput.value = "";
+    displayItems();
+  }
+}
+
+function deleteAllItems() {}
+
+function saveToLocalStorage() {}
+
+function displayItems() {}
